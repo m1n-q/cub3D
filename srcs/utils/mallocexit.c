7028ee4c281343168b3e4d823c545099a045dbd9
m1n-q <m1n_q@naver.com>
2022-01-18 16:10:59 +0900

    err_exit when alloc failed

--- srcs/utils/mallocexit.c
+++ srcs/utils/mallocexit.c
@@ -6,18 +6,19 @@
 /*   By: mishin <mishin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:17:22 by mishin            #+#    #+#             */
-/*   Updated: 2022/01/18 15:22:47 by mishin           ###   ########.fr       */
+/*   Updated: 2022/01/18 16:10:10 by mishin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
 void	*calloc_(size_t count, size_t size)
 {
 	void	*ret;
 
 	ret = ft_calloc(count, size);
 	if (!ret)
-		exit(-1);
+		err_exit("allocation failed", NULL);
 	return (ret);
 }
 
@@ -29,7 +30,7 @@ char	*strdup_(char *s)
 		return (s);
 	ret = ft_strdup(s);
 	if (!ret)
-		exit(-1);
+		err_exit("allocation failed", NULL);
 	return (ret);
 }
 
@@ -39,7 +40,7 @@ char	*strjoin_(const char *s1, const char *s2)
 
 	ret = ft_strjoin(s1, s2);
 	if (!ret)
-		exit(-1);
+		err_exit("allocation failed", NULL);
 	return (ret);
 }
 
@@ -51,7 +52,7 @@ char	*substr_(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	ret = ft_substr(s, start, len);
 	if (!ret)
-		exit(-1);
+		err_exit("allocation failed", NULL);
 }
 
-//TODO: trim, atoi, split
+//TODO: trim, split
